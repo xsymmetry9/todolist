d498329cd21c629f0ca5fe6321319a1b75c055a0
Gary <glswim@gmail.com>
2023-08-22 23:25:11 +0800

    Add code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,4 @@
 import '../styles/style.css';
-import initializeWebsite from './website';
 import UI from "./components/UI";
 
 addEventListener("DOMContentLoaded", UI.loadPage());
-// addEventListener("DOMContentLoaded", initializeWebsite);
